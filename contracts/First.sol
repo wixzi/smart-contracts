00c77b4ff405a5876fd4c4565275159c36f8cdd7
Wixzi <102727631+wixzi@users.noreply.github.com>
2022-04-01 09:39:15 +0200

    First solidity contract

---
+++ contracts/First.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT-License
+pragma solidity ^0.8.0;
+
+contract First {
+    function hello() public pure returns (string memory) {
+        return "Hello, First Contract";
+    }
+}
